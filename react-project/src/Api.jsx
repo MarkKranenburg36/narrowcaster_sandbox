be53ef0f326aad0d185bfae9e9e5c50347352155
Mark <m.kranenburg@qquest.nl>
2024-06-21 12:44:52 +0200

    organise and render weather data

---
+++ react-project/src/Api.jsx
@@ -0,0 +1,8 @@
+export const fetchWeatherData = async () => {
+    const response = await fetch('https://api.weatherapi.com/v1/current.json?key=fe8cc7ce1859439baab125140241806&q=Utrecht');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+};
--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { fetchWeatherData } from './Api';
 
 function App() {
   const [weatherData, setWeatherData] = useState([]);
@@ -21,13 +22,28 @@ function App() {
     return <>Fetching data...</>;
   }
 
+  const daysOfWeek = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
+  const monthsOfYear = [
+    "Januari", "Februari", "Maart", "April", "Mei", "Juni",
+    "Juli", "Augustus", "September", "Oktober", "November", "December"
+  ];
+  const currentDate = new Date();
+  const currentDayOfWeek = daysOfWeek[currentDate.getDay()];
+  const currentMonth = monthsOfYear[currentDate.getMonth()];
+  const localTemp = weatherData.current.temp_c.toString().slice(0,2) + '°C';
+  const weatherIcon = weatherData.current.condition.icon;
+
+
+  console.log(weatherData);
+
   return (
     <>
       <h1>weatherData</h1>
       <ul>
-        {weatherData.map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
+        <li>{currentDayOfWeek}</li>
+        <li>{currentDate.getDate()} {currentMonth}</li>
+        <li>{localTemp}</li>
+        <li><img src={weatherIcon} /></li>
       </ul>
     </>
   );
