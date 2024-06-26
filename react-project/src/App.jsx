0f4c0bad2aefcc0c0246006f6c7696288eca270c
Mark <markkranenburg@hotmail.nl>
2024-06-26 17:31:10 +0200

    add widgetContainer component

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -3,19 +3,20 @@ import { Weather } from './Components/Weather';
 import { Greeting } from './Components/Greeting';
 import Widget from './Components/Widget';
 import { Logo } from './Components/Logo';
-import NSlogo  from './assets/NS-logo.png';
+import NSlogo from './assets/NS-logo.png';
 import { FunFact } from './Components/FunFact';
+import WidgetContainer from './Components/WidgetContainer';
 
 function App() {
   const getGreeting = () => {
     let greeting = (
-    getTimeOfDay() == 'morning' ?
-    'Goede morgen!'
-    :
-    getTimeOfDay() == 'afternoon'
-    ? 'Goede middag!'
-    : 
-    'Goede avond!'
+      getTimeOfDay() == 'morning' ?
+        'Goede morgen!'
+        :
+        getTimeOfDay() == 'afternoon'
+          ? 'Goede middag!'
+          :
+          'Goede avond!'
     )
     return greeting;
   }
@@ -25,7 +26,7 @@ function App() {
     let period = time < 12 ? 'morning' : (time >= 12 && time < 18 ? 'afternoon' : 'evening');
     return period;
   }
-  
+
   return (
     <div id='widgetContainer'>
       <Greeting text={getGreeting()} />
@@ -37,16 +38,23 @@ function App() {
       </Widget>
       <Widget title={'Nieuwsbrief'} className={'widget-center-left'}>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque vero ut veritatis ratione nihil ipsam quas iure dolorum repellat
-           corporis, provident ipsa quod asperiores laborum. Porro consequuntur soluta quis beatae.</p>
+          corporis, provident ipsa quod asperiores laborum. Porro consequuntur soluta quis beatae.</p>
       </Widget>
       <Widget title={'Vandaag'} className={'widget-center-right-top'}>
-        <FunFact>
-
-        </FunFact>
-        <p>Gefeliciteeerd!</p>
+        <WidgetContainer title={'Weetje!'} bgColor={'bgWhite'}>
+          <FunFact />
+        </WidgetContainer>
+        <WidgetContainer title={'Gefeliciteerd'} bgColor={'bgWhite'}>
+          <p>henk & dirk!</p>
+        </WidgetContainer>
       </Widget>
       <Widget title={'Events'} className={'widget-center-right-bottom'}>
-
+      <WidgetContainer bgColor={'bgBlue'}>
+          <p>Event A</p>
+        </WidgetContainer>
+        <WidgetContainer bgColor={'bgBlue'}>
+          <p>Event B</p>
+        </WidgetContainer>
       </Widget>
       <Widget title={'Utrecht CS'} className={'widget-right bg-yellow'} NSlogo={NSlogo}>
 
